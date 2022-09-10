1f261ebb716ac623c042a4a8be88b7a0cf13cc38
Nick <nelom3@gmail.com>
2022-09-10 12:14:44 -0400

    Styles the about page

--- library-frontend/src/components/About.js
+++ library-frontend/src/components/About.js
@@ -2,7 +2,23 @@ import React from "react";
 
 const About = () => {
   return (
-    <div id="about">ABOUT PAGE</div>
+    <div id="about">
+      <p>
+        <b>Book Tracker Is A Book Cataloging Application</b>
+      </p>
+
+      <ul className="ul-align">
+        <li>Simple To Use</li>
+        <li>Intuitive Design</li>
+        <li>Decluttered UI</li>
+        <li>Clean And Minimalistic</li>
+      </ul>
+
+      <p>
+        <b>Create A Genre And Add Books</b>
+      </p>
+        
+    </div>
   )
 }
 
